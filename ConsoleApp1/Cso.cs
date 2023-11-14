a5ef30525b27068b996df57b0e6fb5065ecf473b
Géczi Kornél <hungarokornel@gmail.com>
2023-11-14 11:40:10 +0100

    Add project files.

---
+++ ConsoleApp1/Cso.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Cso : Henger
+    {
+        double falVastagsag;
+        public Cso(double magassag, double sugar, double falVastagsag) : base(magassag, sugar)
+        {
+            this.falVastagsag = falVastagsag;
+        }
+        public Cso(double magassag, double sugar) : base(magassag, sugar)
+        {
+            this.falVastagsag = 1;
+        }
+
+        public double FalVastagsag { get => falVastagsag; }
+
+        public override double Terfogat()
+        {
+            double terfogat1 = Math.Round(Math.PI * base.Sugar * base.Sugar * base.Magassag, 2);
+            double terfogat2 = Math.Round(Math.PI * Math.Pow(base.Sugar - this.falVastagsag, 2) * base.Magassag, 2);
+            return Math.Round(terfogat1 - terfogat2, 2);
+        }
+
+        public override string ToString()
+        {
+            return $"Jellemzők >> térfogat: {this.Terfogat()}; sugár: {base.Sugar}; magasság: {base.Magassag}; falvastagság: {this.falVastagsag}";
+        }
+
+    }
+}
---
+++ ConsoleApp1/Henger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Henger
+    {
+        double magassag, sugar;
+        static double szuletesSzamlalo;
+
+        public Henger(double sugar, double magassag)
+        {
+            this.magassag = magassag;
+            this.sugar = sugar;
+            szuletesSzamlalo += 1;
+        }
+
+        public double Magassag { get => magassag; }
+        public double Sugar { get => sugar; }
+        public static double SzuletesSzamlalo { get => szuletesSzamlalo; }
+
+        public virtual double Terfogat()
+        {
+            return Math.Round(sugar * sugar * Math.PI * magassag, 2);
+        }
+
+        public override string ToString()
+        {
+            return $"Jellemzők >> térfogat: {this.Terfogat()}; sugár: {this.sugar}; magasság: {this.magassag}";
+        }
+    }
+}
